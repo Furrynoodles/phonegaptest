439bab3b9b78fff3cfef452561b54b9e9c5bc5fe
Clarence Lee <clarencedglee@gmail.com>
2013-12-05 22:32:52 +0000

    remove build files

--- hello/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-F:\Freelance\BCT\app\phonegaptest\hello\platforms\android\bin\AndroidManifest.xml : \
-F:\Freelance\BCT\app\phonegaptest\hello\platforms\android\AndroidManifest.xml \
--- hello/platforms/android/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-F:\Freelance\BCT\app\phonegaptest\hello\platforms\android\gen\com\example\hello\R.java \
- : F:\Freelance\BCT\app\phonegaptest\hello\platforms\android\res\drawable\icon.png \
-F:\Freelance\BCT\app\phonegaptest\hello\platforms\android\res\drawable-hdpi\icon.png \
-F:\Freelance\BCT\app\phonegaptest\hello\platforms\android\res\drawable-ldpi\icon.png \
-F:\Freelance\BCT\app\phonegaptest\hello\platforms\android\res\drawable-mdpi\icon.png \
-F:\Freelance\BCT\app\phonegaptest\hello\platforms\android\res\drawable-xhdpi\icon.png \
-F:\Freelance\BCT\app\phonegaptest\hello\platforms\android\res\values\strings.xml \
-F:\Freelance\BCT\app\phonegaptest\hello\platforms\android\res\xml\config.xml \
-F:\Freelance\BCT\app\phonegaptest\hello\platforms\android\bin\AndroidManifest.xml \
